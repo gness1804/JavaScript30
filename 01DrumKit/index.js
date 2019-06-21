c31cb97394d2c34b9bbdfe4609414c91187fa3fe
gness1804 <graham.nessler@gmail.com>
2019-06-20 20:50:19 -0500

    Got sound working

--- 01DrumKit/index.js
+++ 01DrumKit/index.js
@@ -1,5 +1,20 @@
+const playSound = async (item) => {
+  try {
+    await item.play();
+  } catch (error) {
+    throw new Error(`Problem playing sound: ${error}`);
+  }
+};
+
 document.addEventListener('keyup', (e) => {
   const keyPressed = e.which;
+
+  document.querySelectorAll('audio').forEach(item => {
+    if (parseInt(item.dataset.key, 10) === keyPressed) {
+      playSound(item);
+    }
+  });
+
   document.querySelectorAll('.key').forEach(item => {
     if (parseInt(item.dataset.key, 10) === keyPressed) {
       item.classList.add('playing');
