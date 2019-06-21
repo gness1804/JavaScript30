2cd2b9a95aca2ad11cc87613bc7ccd14ba0363bc
gness1804 <graham.nessler@gmail.com>
2019-06-20 20:29:39 -0500

    Install eslint and change name of first lesson dir

--- 01DrumKit/index.js
+++ 01DrumKit/index.js
@@ -1,8 +1,8 @@
 document.addEventListener('keyup', (e) => {
-  const keyPressed = event.which;
+  const keyPressed = e.which;
   document.querySelectorAll('.key').forEach(item => {
     if (parseInt(item.dataset.key, 10) === keyPressed) {
       item.classList.add('playing');
     }
-  })
+  });
 });
