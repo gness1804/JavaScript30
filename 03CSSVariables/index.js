7115c0f8ba82cb7f3bc87870276a44e5d376f6d3
gness1804 <graham.nessler@gmail.com>
2019-06-23 18:23:48 -0500

    got color picker working

--- 03CSSVariables/index.js
+++ 03CSSVariables/index.js
@@ -1,6 +1,6 @@
 const spacingBar = document.getElementById('spacing');
 const blurBar = document.getElementById('blur');
-// const baseDisplay = document.getElementById('base');
+const colorPicker = document.getElementById('base');
 const root = document.documentElement;
 
 spacingBar.addEventListener('change', (e) => {
@@ -11,4 +11,9 @@ spacingBar.addEventListener('change', (e) => {
 blurBar.addEventListener('change', (e) => {
   const val = e.target.value;
   root.style.setProperty('--blur', `${val}px`);
+});
+
+colorPicker.addEventListener('change', (e) => {
+  const val = e.target.value;
+  root.style.setProperty('--base', `${val}`);
 });
