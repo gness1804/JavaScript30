e8119f74f01a200b81d30e8d44889617cc6a7a62
gness1804 <graham.nessler@gmail.com>
2019-06-23 18:57:53 -0500

    Break handleUpdate into own function

--- 03CSSVariables/index.js
+++ 03CSSVariables/index.js
@@ -1,15 +1,17 @@
 const inputs = document.querySelectorAll('.controls input');
 const root = document.documentElement;
 
+const handleUpdate = (e, input) => {
+  const { value } = e.target;
+  const { id } = input;
+  const { sizing } = input.dataset;
+  if (sizing) {
+    root.style.setProperty(`--${id}`, `${value}${sizing}`);
+  } else {
+    root.style.setProperty(`--${id}`, `${value}`);
+  }
+};
+
 inputs.forEach(input => {
-  input.addEventListener('change', (e) => {
-    const val = e.target.value;
-    const { id } = input;
-    const { sizing } = input.dataset;
-    if (sizing) {
-      root.style.setProperty(`--${id}`, `${val}${sizing}`);
-    } else {
-      root.style.setProperty(`--${id}`, `${val}`);
-    }
-  });
+  input.addEventListener('change', (e) => handleUpdate(e, input));
 });
