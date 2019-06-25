ade2833a01a69b7e58086eef0d756459f16e73c0
gness1804 <graham.nessler@gmail.com>
2019-06-24 20:12:55 -0500

    Destructure inventor properties in first two problems

--- 04ArrayCardioDay1/index.js
+++ 04ArrayCardioDay1/index.js
@@ -53,12 +53,12 @@ const inventors = [
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
-const filterInventors = () => inventors.filter(inventor => inventor.year >= 1500 && inventor.year < 1600);
+const filterInventors = () => inventors.filter(({ year }) => year >= 1500 && year < 1600);
 console.info('The filtered inventors:');
 console.table(filterInventors());
 // Array.prototype.map()
 // 2. Give us an array of the inventors' first and last names
-const mapInventors = () => inventors.map(inventor => `${inventor.first} ${inventor.last}`);
+const mapInventors = () => inventors.map(({ first, last }) => `${first} ${last}`);
 console.info('The mapped inventors:');
 console.table(mapInventors());
 
