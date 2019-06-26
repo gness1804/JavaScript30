b661a3819489b142d33d401a890a417a08cca0e2
gness1804 <graham.nessler@gmail.com>
2019-06-26 17:51:41 -0500

    Sort people by last name - problem 7 solved

--- 04ArrayCardioDay1/index.js
+++ 04ArrayCardioDay1/index.js
@@ -43,13 +43,13 @@ const inventors = [
   },
 ];
 
-// const people = [
-//   'Beck, Glenn',
-//   'Becker, Carl',
-//   'Beckett, Samuel',
-//   'Beddoes, Mick',
-//   'Beecher, Henry',
-//   'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
+const people = [
+  'Beck, Glenn',
+  'Becker, Carl',
+  'Beckett, Samuel',
+  'Beddoes, Mick',
+  'Beecher, Henry',
+  'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
@@ -136,6 +136,10 @@ console.table(extractTheBoulevardsWithDe());
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
+const sortPeopleByLastName = () => people.sort();
+
+console.info('The sorted people');
+console.table(sortPeopleByLastName());
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
