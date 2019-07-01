a18023d992b4fea81462eeba187ab70e06a4ac05
gness1804 <graham.nessler@gmail.com>
2019-06-30 21:31:37 -0500

    Got MVP--listed results are showing up

--- 06TypeAhead/index.js
+++ 06TypeAhead/index.js
@@ -1,4 +1,5 @@
 const searchBox = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 const cities = [];
 
@@ -9,7 +10,13 @@ fetch(endpoint)
 const findMatches = () => {
   const searchBoxVal = searchBox.value.toLowerCase();
   const filteredResults = cities.filter(res => res.city.toLowerCase().includes(searchBoxVal) || res.state.toLowerCase().includes(searchBoxVal));
-
+  const html = filteredResults.map(cityObj => `
+    <li>
+      <span class="name">${cityObj.city}, ${cityObj.state}</span>
+      <span class="population">${cityObj.population}</span>
+    <li>
+  `).join('');
+  suggestions.innerHTML = html;
 };
 
 searchBox.addEventListener('keyup', findMatches);
