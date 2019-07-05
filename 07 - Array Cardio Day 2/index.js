6c23f4559e47db28cf7f9af14c83f8e13aebd2fa
gness1804 <graham.nessler@gmail.com>
2019-07-05 18:21:12 -0500

    Solve problem 2 of exercise 7

--- 07 - Array Cardio Day 2/index.js
+++ 07 - Array Cardio Day 2/index.js
@@ -28,6 +28,13 @@ const isSomeoneOfAge = arr => arr.some(person => thisYear - person.year >= 19);
 console.info(`Are one or more of the people 19 or older? ${isSomeoneOfAge(people) ? 'Yes' : 'No'}`);
 
 // Array.prototype.every() // is everyone 19 or older?
+/**
+ *
+ * @param {{ name: string, year: number}[]} arr - a list of people
+ * @returns {boolean} - whether or not all of the people are of age
+ */
+const isEveryoneOfAge = arr => arr.every(person => thisYear - person.year >= 19);
+console.info(`Are all of the people 19 or older? ${isEveryoneOfAge(people) ? 'Yes' : 'No'}`);
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
