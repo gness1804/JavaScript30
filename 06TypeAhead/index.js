874f5b2e5de94f5d9ee46b6a31e77abe491de3d6
gness1804 <graham.nessler@gmail.com>
2019-07-05 17:01:22 -0500

    Finish with exercise 6

--- 06TypeAhead/index.js
+++ 06TypeAhead/index.js
@@ -8,12 +8,19 @@ fetch(endpoint)
   .then(res => cities.push(...res));
 
 /**
-   *
+   * Takes an input full name string of a city or state and adds formatting to the portion that matches the search text
    * @param {string} fullNameStr - the full city or state name: Example: "Chicago"
    * @param {string} searchBoxVal - the value entered into the search box, usually a substring of the city/state name. Example: "chi"
    * @returns {string} - the formatted string to display
    */
 const highlightResult = (fullNameStr, searchBoxVal) => fullNameStr.replace(new RegExp(`${searchBoxVal}`, 'gi'), `<span class="hl">${searchBoxVal}</span>`);
+
+/**
+ * Formats a population figure by adding commas for thousands. Thanks to the following source for the regex: https://coderwall.com/p/uccfpq/formatting-currency-via-regular-expression
+ * @param {string} pop - the population, which comes in as a string. Example: '235980'
+ * @returns {string} - the same population number, but with commas added for readability. Example: '235,980'
+ */
+const addCommasToPopulation = (pop) => pop.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
 
 const findMatches = () => {
   const searchBoxVal = searchBox.value;
@@ -21,7 +28,7 @@ const findMatches = () => {
   const html = filteredResults.map(cityObj => `
     <li>
       <span class="name">${highlightResult(cityObj.city, searchBoxVal)}, ${highlightResult(cityObj.state, searchBoxVal)}</span>
-      <span class="population">${cityObj.population}</span>
+      <span class="population">${addCommasToPopulation(cityObj.population)}</span>
     <li>
   `).join('');
   suggestions.innerHTML = html;
