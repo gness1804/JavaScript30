3c22ab450b5a8612bc91da71903f0340f6e129e9
gness1804 <graham.nessler@gmail.com>
2019-07-07 18:10:10 -0500

    Add varying color with draw and line height of 100

--- 08 - Fun with HTML5 Canvas/index.js
+++ 08 - Fun with HTML5 Canvas/index.js
@@ -5,19 +5,23 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#bada55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 100;
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
 
 const draw = (e) => {
   if (isDrawing) {
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
     const { offsetX, offsetY } = e;
     ctx.beginPath();
     ctx.moveTo(lastX, lastY);
     ctx.lineTo(offsetX, offsetY);
     ctx.stroke();
     [lastX, lastY] = [offsetX, offsetY];
+    hue++;
   }
 };
 
