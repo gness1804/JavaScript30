f2184ac6360f2591a4be53f26bbaeeaa046d6a02
gness1804 <graham.nessler@gmail.com>
2019-07-07 21:56:06 -0500

    Intial setup and some console statements for exer 9

---
+++ 09 - Dev Tools Domination/index.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-console */
+// const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
+
+/* eslint-disable no-unused-vars */
+const makeGreen = () => {
+  const p = document.querySelector('p');
+  p.style.color = '#BADA55';
+  p.style.fontSize = '50px';
+};
+/* eslint-enable no-unused-vars */
+
+// Regular
+console.log('sanity check');
+
+// Interpolated
+console.log('My name is %s', 'Iron Man');
+
+// Styled
+console.log('%c Iron Man text', 'color: #f00');
+// warning!
+console.warn('Warning: something bad might happen!');
+
+// Error :|
+
+// Info
+
+// Testing
+
+// clearing
+
+// Viewing DOM Elements
+
+// Grouping together
+
+// counting
+
+// timing
