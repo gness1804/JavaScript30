76079a57bba1f029cbaa5a1a7538f35a53cac562
gness1804 <graham.nessler@gmail.com>
2019-07-12 21:05:12 -0500

    Refactor by creating handleCheck function

--- 10 - Hold Shift and Check Checkboxes/index.js
+++ 10 - Hold Shift and Check Checkboxes/index.js
@@ -1,21 +1,26 @@
 const checkboxes = Array.from(document.querySelectorAll('.item input'));
-let lastCheckedIndex = null;
+let lastChecked;
+
+// old-school es5 function needed in order to capture proper value of "this"
+function handleCheck(e) {
+  const thisBox = this;
+  const isChecked = thisBox.checked;
+  if (isChecked) {
+    if (lastChecked !== null && e.shiftKey) {
+      const thisIndex = checkboxes.indexOf(thisBox);
+      const lastCheckedIndex = checkboxes.indexOf(lastChecked);
+      checkboxes.forEach((_box) => {
+        if (checkboxes.indexOf(_box) > lastCheckedIndex && checkboxes.indexOf(_box) < thisIndex) {
+          _box.checked = true;
+        } else if (checkboxes.indexOf(_box) < lastCheckedIndex && checkboxes.indexOf(_box) > thisIndex) {
+          _box.checked = true;
+        }
+      });
+    }
+    lastChecked = this;
+  }
+}
 
 checkboxes.forEach((box) => {
-  box.addEventListener('click', (e) => {
-    const isChecked = e.target.checked;
-    if (isChecked) {
-      if (lastCheckedIndex !== null && e.shiftKey) {
-        const thisIndex = checkboxes.indexOf(box);
-        checkboxes.forEach((_box) => {
-          if (checkboxes.indexOf(_box) > lastCheckedIndex && checkboxes.indexOf(_box) < thisIndex) {
-            _box.checked = true;
-          } else if (checkboxes.indexOf(_box) < lastCheckedIndex && checkboxes.indexOf(_box) > thisIndex) {
-            _box.checked = true;
-          }
-        });
-      }
-      lastCheckedIndex = checkboxes.indexOf(box);
-    }
-  });
+  box.addEventListener('click', handleCheck);
 });
