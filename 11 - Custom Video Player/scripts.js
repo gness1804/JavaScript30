ad2174512be2175ad8f1b2a71299c2fe12b7fad3
gness1804 <graham.nessler@gmail.com>
2019-07-14 18:26:26 -0500

    Restore play icon and toggle play/pause icons more intelligently with updateButtonDisplay that is decoupled from the play/pause function

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -7,10 +7,16 @@ const rewindButton = player.querySelector('.player__button.rewind');
 const fastForwardButton = player.querySelector('.player__button.fast-forward');
 const video = player.querySelector('.player__video.viewer');
 
-/**
+/** toggles between play and pause
  * @returns {Function} - video.play() or video.pause() depending on video's current state
  */
 const handlePlayPause = () => video[video.paused ? 'play' : 'pause']();
+
+function updateButtonDisplay() {
+  const playIcon = '►';
+  const pauseIcon = '❚ ❚';
+  playOrPauseButton.innerText = this.paused ? playIcon : pauseIcon;
+}
 
 function handleVolumeChange() {
   const newVol = this.value;
@@ -41,3 +47,5 @@ fastForwardButton.addEventListener('click', handleFastForward);
 rewindButton.addEventListener('click', handleRewind);
 video.addEventListener('click', handlePlayPause);
 video.addEventListener('dblclick', resetVideo);
+video.addEventListener('play', updateButtonDisplay);
+video.addEventListener('pause', updateButtonDisplay);
