d36e9eac5d28c012951e7646f2681ebfd9519a83
gness1804 <graham.nessler@gmail.com>
2019-07-14 16:25:19 -0500

    Delete finished scripts file and set up var selectors for exer 11

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,7 @@
+const progressBar = document.querySelector('.progress');
+const playOrPauseButton = document.querySelector('.player__button toggle');
+const volumeSlider = document.querySelector('.player__slider.volume');
+const playbackRateSlider = document.querySelector('.player__slider.playback-rate');
+const rewindButton = document.querySelector('.player__button.rewind');
+const fastForwardButton = document.querySelector('.player__button.fast-forward');
+
