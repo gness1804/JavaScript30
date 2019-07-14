cd5cc4d7c3b64551b64b236393fab71560e900c2
gness1804 <graham.nessler@gmail.com>
2019-07-14 16:46:16 -0500

    Got basic play/pause functionality working

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,7 +1,19 @@
-const progressBar = document.querySelector('.progress');
-const playOrPauseButton = document.querySelector('.player__button toggle');
-const volumeSlider = document.querySelector('.player__slider.volume');
-const playbackRateSlider = document.querySelector('.player__slider.playback-rate');
-const rewindButton = document.querySelector('.player__button.rewind');
-const fastForwardButton = document.querySelector('.player__button.fast-forward');
+// const progressBar = document.querySelector('.progress');
+const playOrPauseButton = document.querySelector('.player__button.toggle');
+// const volumeSlider = document.querySelector('.player__slider.volume');
+// const playbackRateSlider = document.querySelector('.player__slider.playback-rate');
+// const rewindButton = document.querySelector('.player__button.rewind');
+// const fastForwardButton = document.querySelector('.player__button.fast-forward');
+const video = document.querySelector('.player__video.viewer');
 
+const handlePlayPause = () => {
+  if (video.paused) {
+    video.play();
+    playOrPauseButton.innerText = 'Pause';
+  } else {
+    video.pause();
+    playOrPauseButton.innerText = 'Play';
+  }
+};
+
+playOrPauseButton.addEventListener('click', handlePlayPause);
