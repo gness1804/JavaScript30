a179059be9d99ec12d4eca029f4b0f177067fb0b
gness1804 <graham.nessler@gmail.com>
2019-07-15 21:17:56 -0500

    Refactor to break out new sequence and add to sequence into own methods

--- 12 - Key Sequence Detection/index.js
+++ 12 - Key Sequence Detection/index.js
@@ -2,30 +2,37 @@ let lastKeyupTime;
 let targetStr = '';
 
 const handleKeyup = (e) => {
+  const startNewSequence = () => {
+    const keyPressed = e.key;
+    targetStr = keyPressed;
+    lastKeyupTime = Date.now();
+  };
+
+  const addKeyToSequence = () => {
+    const keyPressed = e.key;
+    targetStr += keyPressed;
+    lastKeyupTime = Date.now();
+  };
+
   if (targetStr === 'cats') {
     targetStr = '';
     alert('Congratulations! You win!');
     return;
   }
+
   if (lastKeyupTime) {
     const timeElapsed = Date.now() - lastKeyupTime;
     if (timeElapsed > 3000) {
       // start a new sequence if the lag between presses is more than 3 seconds
-      const keyPressed = e.key;
-      targetStr = keyPressed;
-      lastKeyupTime = Date.now();
+      startNewSequence();
       return;
     }
     // else, add this key to the string of keys pressed in a row
-    const keyPressed = e.key;
-    targetStr += keyPressed;
-    lastKeyupTime = Date.now();
+    addKeyToSequence();
     return;
   }
   // start a new sequence since this is the first key pressed
-  const keyPressed = e.key;
-  targetStr = keyPressed;
-  lastKeyupTime = Date.now();
+  startNewSequence();
 };
 
 window.addEventListener('keyup', handleKeyup);
