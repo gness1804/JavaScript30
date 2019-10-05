043e2ba4b53a0b5827aae847e17751d1b589897a
gness1804 <graham.nessler@gmail.com>
2019-10-05 15:21:00 -0500

    Finish exer 13

--- 13 - Slide in on Scroll/index.js
+++ 13 - Slide in on Scroll/index.js
@@ -1,8 +1,7 @@
-function debounce(func, wait = 20, immediate = true) {
+function debounce(func, wait = 20, immediate = true, ...args) {
   let timeout;
   return function() {
     const context = this;
-    const args = arguments;
     const later = function() {
       timeout = null;
       if (!immediate) func.apply(context, args);
@@ -13,3 +12,25 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+const checkSlide = () => {
+  sliderImages.forEach(img => {
+    // the bottom of the screen
+    const screenBottom = window.scrollY + window.innerHeight;
+    // halfway through the image vertically
+    const slideInAtPos = screenBottom - img.height / 2;
+    // the bottom of the image.
+    const imageBottom = img.offsetTop + img.height;
+    const isHalfShown = slideInAtPos > img.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      img.classList.add('active');
+    } else {
+      img.classList.remove('active');
+    }
+  });
+};
+
+document.addEventListener('scroll', debounce(checkSlide, 100));
