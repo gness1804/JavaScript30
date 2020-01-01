f8c61265f9db32bfcdb53b137715e5f6880690a6
gness1804 <graham.nessler@gmail.com>
2020-01-01 13:22:45 -0600

    Got bands sorted and displaying correctly for exer 17
    Add whitespace to regex

---
+++ 17 - Sort Without Articles/index.js
@@ -0,0 +1,34 @@
+const listElem = document.querySelector('#bands');
+
+const bands = [
+  'The Plot in You',
+  'The Devil Wears Prada',
+  'Pierce the Veil',
+  'Norma Jean',
+  'The Bled',
+  'Say Anything',
+  'The Midway State',
+  'We Came as Romans',
+  'Counterparts',
+  'Oh, Sleeper',
+  'A Skylit Drive',
+  'Anywhere But Here',
+  'An Old Dog',
+];
+
+const regex = /^(The|An?)\s/i;
+
+const sortedBands = bands.sort((a, b) => {
+  const strippedA = a.replace(regex, '').trim();
+  const strippedB = b.replace(regex, '').trim();
+
+  if (strippedA < strippedB) {
+    return -1;
+  }
+  if (strippedA > strippedB) {
+    return 1;
+  }
+  return 0;
+});
+
+listElem.innerHTML = sortedBands.map(band => `<li>${band}</li>`);
