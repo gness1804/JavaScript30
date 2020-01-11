adc69fa40860d36fda5dd92976aa2e6101dff4a9
gness1804 <graham.nessler@gmail.com>
2020-01-11 17:28:04 -0600

    Initial setup for exer 19 and got video playing

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,6 +1,19 @@
-/* eslint-disable */ /* remove when I start the lesson */
 const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+const getVideo = () => {
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true,
+      audio: false,
+    })
+    .then(stream => {
+      video.srcObject = stream;
+      video.play();
+    });
+};
+
+getVideo();
