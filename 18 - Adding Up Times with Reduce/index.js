0796e3433acb6d8c635aa6ad6b51a005e6a44869
gness1804 <graham.nessler@gmail.com>
2020-01-11 16:01:42 -0600

    Make convertRawSecToTimeDisplay much shorter and cleaner and add identifier for each test. Finish with exer 18.

--- 18 - Adding Up Times with Reduce/index.js
+++ 18 - Adding Up Times with Reduce/index.js
@@ -1,30 +1,10 @@
 const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
 
 const convertRawSecToTimeDisplay = secs => {
-  let hours = 0;
-  let minutes = 0;
-  let seconds = 0;
-
-  if (secs >= 3600) {
-    // at least one hour
-    hours = Math.floor(secs / 3600);
-    const leftoverMinSec = secs % 3600;
-    if (leftoverMinSec >= 60) {
-      // the leftover is equal to or greater than one minute; need to break into minutes:seconds
-      minutes = Math.floor(leftoverMinSec / 60);
-      seconds = leftoverMinSec % 60;
-    } else {
-      // leftover is less than one minute
-      seconds = leftoverMinSec;
-    }
-  } else if (secs >= 60) {
-    // the leftover is equal to or greater than one minute; need to break into minutes:seconds
-    minutes = Math.floor(secs / 60);
-    seconds = secs % 60;
-  } else {
-    // less than one minute.
-    seconds = secs;
-  }
+  const hours = Math.floor(secs / 3600);
+  const leftOverSeconds = secs % 3600;
+  const minutes = Math.floor(leftOverSeconds / 60);
+  const seconds = leftOverSeconds % 60;
 
   return `${hours.toString()}:${minutes.toString()}:${seconds.toString()}`;
 };
@@ -48,19 +28,19 @@ console.log(`The total time for all the videos is: ${result}`);
 
 // rudimentary tests to make sure the time conversion function is correct
 if (convertRawSecToTimeDisplay(3662) !== '1:1:2') {
-  throw new Error('Error with convertRawSecToTimeDisplay call');
+  throw new Error('Error with convertRawSecToTimeDisplay call for 3662 secs');
 }
 
 if (convertRawSecToTimeDisplay(42) !== '0:0:42') {
-  throw new Error('Error with convertRawSecToTimeDisplay call');
+  throw new Error('Error with convertRawSecToTimeDisplay call for 42 secs');
 }
 
 if (convertRawSecToTimeDisplay(312) !== '0:5:12') {
-  throw new Error('Error with convertRawSecToTimeDisplay call');
+  throw new Error('Error with convertRawSecToTimeDisplay call for 312 secs');
 }
 
 if (convertRawSecToTimeDisplay(6732) !== '1:52:12') {
-  throw new Error('Error with convertRawSecToTimeDisplay call');
+  throw new Error('Error with convertRawSecToTimeDisplay call for 6732 secs');
 }
 
 if (convertRawSecToTimeDisplay(1734) !== '0:28:54') {
