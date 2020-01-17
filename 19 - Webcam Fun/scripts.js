7a723a5792f3a624361d9a700d60c3270052be49
gness1804 <graham.nessler@gmail.com>
2020-01-16 19:58:34 -0600

    Add red effect

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -4,6 +4,15 @@ const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
+const redEffect = pixels => {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; // red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // green
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // yellow
+  }
+  return pixels;
+};
+
 const paintToCanvas = () => {
   const { videoWidth: width } = video;
   const { videoHeight: height } = video;
@@ -12,6 +21,9 @@ const paintToCanvas = () => {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    pixels = redEffect(pixels);
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 };
 
