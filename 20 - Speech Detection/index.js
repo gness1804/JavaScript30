fdc57f5e0cd440f080fcc49477732501c3b09f15
gness1804 <graham.nessler@gmail.com>
2020-01-18 15:25:14 -0600

    Got MVP for exer 20

--- 20 - Speech Detection/index.js
+++ 20 - Speech Detection/index.js
@@ -5,17 +5,24 @@ window.SpeechRecognition =
 const recognition = new SpeechRecognition();
 recognition.interimResults = true;
 
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(p);
+
 const handleResult = e => {
   const transcript = Array.from(e.results)
     .map(res => res[0])
     .map(res => res.transcript)
     .join(' ');
+
+  p.textContent = transcript;
+  if (e.results[0].isFinal) {
+    p = document.createElement('p');
+    words.appendChild(p);
+  }
 };
 
-let p = document.createElement('p');
-const words = document.querySelector('.words');
-words.appendChild(p);
-
 recognition.addEventListener('result', handleResult);
+recognition.addEventListener('end', recognition.start);
 
 recognition.start();
